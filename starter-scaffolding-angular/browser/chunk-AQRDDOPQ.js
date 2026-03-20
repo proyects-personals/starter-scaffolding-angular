2d89d6124b40f32ec1086a6f6a85f8662406f1b8
steveencues <83484587+steveencues@users.noreply.github.com>
2026-03-20 07:00:15 +0000

    Deploying to gh-pages from @ proyects-personals/starter-scaffolding-angular@be5579f876d8c0c7d8c112d3765b4f4ffbdb1cea 🚀

---
+++ starter-scaffolding-angular/browser/chunk-AQRDDOPQ.js
@@ -0,0 +1,1 @@
+import{b as a,c as b,d as c,e as d,f as e}from"./chunk-FI6V4LAI.js";export{e as NotFoundScreenComponent,c as Presentation,d as WelcomeComponent,b as appConfig,a as routes};
---
+++ starter-scaffolding-angular/browser/main-6DI7PCYD.js
@@ -0,0 +1,1 @@
+import{a as o,c as r,d as t}from"./chunk-FI6V4LAI.js";o(t,r).catch(p=>console.error(p));
