0dac9657a2bde3c140e7041431e5fa509eeccead
SteveenCues <zh311505@gmail.com>
2026-03-20 01:18:55 -0500

    feat(enviromen): paths

--- src/app/presentation/aplication/controls/404/not-found-screen/not-found-screen.component.ts
+++ src/app/presentation/aplication/controls/404/not-found-screen/not-found-screen.component.ts
@@ -1,4 +1,4 @@
-import { environment } from '@/environments/environment';
+import { environment } from '../../../../../../environments/environment';
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { APP_ROUTES } from '@domain';
